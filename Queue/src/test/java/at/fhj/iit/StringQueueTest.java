5b9ec2f2421e9423ed4b59a32c7ee82287b24c40
Gerald <gerald.gsellmann@edu.fh-joanneum.at>
2019-06-12 17:25:24 +0200

    added new testcases

--- Queue/src/test/java/at/fhj/iit/StringQueueTest.java
+++ Queue/src/test/java/at/fhj/iit/StringQueueTest.java
@@ -1,11 +1,55 @@
 package at.fhj.iit;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.*;
 
 public class StringQueueTest {
 
+    private Queue s;
 
-    /**
-     * TODO Write your own tests
-     */
+    @Before
+    public void setup() throws Exception
+    {
+        s = new StringQueue(5); // keep size of 5 !!
+    }
 
+    @Test
+    public void testPollIsEmpty() throws Exception {
+        Assert.assertNull(s.poll());
+    }
+
+    @Test
+    public void testPollIsNotEmpty() throws Exception {
+        s.offer("Hallo");
+        Assert.assertNotNull(s.poll());
+    }
+
+    @Test
+    public void testPeekIsEmpty() throws Exception {
+        Assert.assertNull(s.peek());
+    }
+
+    @Test
+    public void testPeekIsNotEmpty() throws Exception {
+        s.offer("Hallo");
+        Assert.assertNotNull(s.peek());
+    }
+
+    @Test
+    public void testRemoveIsNotEmpty () {
+        s.offer("Hallo");
+        s.offer("Hallo");
+        Assert.assertNotNull(s.remove());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testNoSuchElementException() {
+        s.element();
+        s.poll();
+    }
 }
